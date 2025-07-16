60ec7953b5fb5dbe1afc18785432f8032a1ebd5d
leo9iota <leo9iota@gmail.com>
2025-07-17 00:34:20 +0200

    Add table module

---
+++ src/ui/table.py
@@ -0,0 +1,10 @@
+def features_table():
+    print("features table")
+
+
+def indentifiers_table():
+    print("identifiers table")
+
+
+def modified_indentifiers_table():
+    print("modified identifiers table")
