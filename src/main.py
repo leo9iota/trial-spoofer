c36dc7f24ed6fa4ebb2471c59f047ce3610a010d
leo9iota <leo9iota@gmail.com>
2025-07-24 17:17:30 +0200

    feat(main): implement entry point

---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Entry point for running VSCode Spoofer directly."""
+
+def main():
+    """Main entry point for the CLI application."""
+    print("hello world")
+    return 0
