f65010741a79053e15d8da1fba624d709f7db3f3
leo9iota <leo9iota@gmail.com>
2025-07-25 21:04:13 +0200

    feat(config): add `Config` class with initialization

--- src/core/config.py
+++ src/core/config.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+
+"""
+Config Class (config.py)
+
+Configuration for the application.
+"""
+
+
+class Config:
+    """Configuration for the application."""
+
+    def __init__(self) -> None:
+        """Initialize the Config class."""
