23228b036d6d72e24a7df40b105cf6d5916f0c42
leo9iota <leo9iota@gmail.com>
2025-07-25 21:04:32 +0200

    feat(core): add `Spoof` class with initialization

---
+++ src/core/spoof.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Spoof Class (spoof.py)
+
+Spoof the system.
+"""
+
+
+class Spoof:
+    """Spoof the system."""
+
+    def __init__(self) -> None:
+        """Initialize the Spoof class."""
