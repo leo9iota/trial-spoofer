55729afa6159cc99398ff8527de58f56a76efc11
leo9iota <leo9iota@gmail.com>
2025-07-25 21:09:51 +0200

    feat(filesystem): enhance `Filesystem` class with initialization and cache deletion methods

--- src/utils/filesystem.py
+++ src/utils/filesystem.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+"""
+Filesystem Class (filesystem.py)
+
+Utilities for spoofing the filesystem.
+"""
+
 
 class Filesystem:
-    print("Filesystem class")
+    """Utilities for spoofing the filesystem."""
+
+    def __init__(self) -> None:
+        """Initialize the Filesystem class."""
+
+    def delete_vscode_cache(self) -> None:
+        """Delete the VS Code cache."""
+
+    def delete_cursor_cache(self) -> None:
+        """Delete the Cursor cache."""
